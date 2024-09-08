383ada90147dab2fe70f063852043a25cf5bfb62
Cyprien De Roef <c.deroef@outlook.fr>
2024-09-08 22:11:30 +0200

    wip: Change theme and sucessfully place UI in front of map

--- app.vue
+++ app.vue
@@ -1,20 +1,34 @@
 <template>
-  <LMap
-    style="height: 100dvh"
-    :zoom="6"
-    :center="[47.21322, -1.559482]"
-    :use-global-leaflet="false"
-  >
-    <LTileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
-      layer-type="base"
-      name="OpenStreetMap"
-      class="z-0 absolute top-0 left-0"
-    />
-    <Input
-      class="z-50 w-96 absolute top-4 left-4"
-      placeholder="Search for a place"
-    />
-</LMap>
+    <LMap
+        class="min-h-screen absolute z-0"
+        :zoom="6"
+        :center="[47.21322, -1.559482]"
+        :use-global-leaflet="false"
+        @ready="onMapReady"
+    >
+        <LTileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+            layer-type="base"
+            name="OpenStreetMap"
+        />
+    </LMap>
+    <div class="p-6">
+        <Input 
+            class="w-80 mx-auto relative"
+            placeholder="Search for an alert or a place"
+        />
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const map = ref(null)
+
+// When the map is ready
+const onMapReady = () => {
+    // Access the Leaflet map instance
+    console.log(map)
+}
+</script>
